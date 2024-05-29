997e50c24698eb9a3530804735a1e6e313cc8caa
jangseoyoung <suhyoung030327@naver.com>
2024-05-29 16:08:21 +0900

    first

---
+++ todo.py
@@ -0,0 +1,18 @@
+form fastapi import APIRouter
+
+todo_router = APIRouter()
+
+todo_list = []
+
+@todo_router.post("/todo")
+async def add_todo(todo:dict) -> dict:
+    todo_list.append(todo)
+    return {
+        "msg" : "todo added successfully"
+    }
+
+@todo_router.get("/todo")
+async def retrieve_todos() -> dict:
+    return {
+        "todos" : todo_list
+    }
